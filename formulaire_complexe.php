fc30cb0525e878580d57f800eed44d1ef717ec7c
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-19 14:13:19 +0200

    text

--- formulaire_complexe.php
+++ formulaire_complexe.php
@@ -5,19 +5,28 @@ require_once "includes/functions/clean_input.php";
 if (isset($_SESSION['users']['id'])) {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $errors = [];
-        if (empty($_POST['titre']) || strlen($_POST['titre']) > 60) {
-            $error = "titre inexistant et ou n'excede pas 60 caracteres";
+        if (empty($_POST['sujet']) || strlen($_POST['sujet']) > 60) {
+            $errors = "sujet inexistant et ou n'excede pas 60 caracteres";
         }
-        $titre = clean_input($_POST['titre']);
-        if (empty($_POST['description']) || strlen($_POST['description']) > 255) {
-            $error = "La description ne doit pas exceder 250 caracteres";
+        $sujet = clean_input($_POST['sujet']);
+        if (empty($_POST['message']) || strlen($_POST['message']) > 255) {
+            $errors = "La message ne doit pas exceder 250 caracteres";
         }
-        $description = clean_input($_POST['description']);
-        $date_limite=clean_input($_POST['deadline']);
-        if(strlen($_POST['etapes'])>255){
-            $error="veuillez reduire le nombre d'etapes";
+        $message = clean_input($_POST['message']);
+        $date_limite = clean_input($_POST['deadline']);
+        if (strlen($_POST['etapes']) > 255 || empty($_POST['etapes'])) {
+            $errors = "veuillez reduire le nombre d'etapes";
         }
-        $etapes=clean_input($_POST['etapes']);
+        $etapes = clean_input($_POST['etapes']);
+        if (empty($errors)) {
+            $sql = $db->prepare("INSERT INTO `taches` (`sujet`, `message`,`date_limite`,`etapes`, `type`) VALUES(:sujet, :message, :date_limite, :etapes, 'complexe') ");
+            $sql->execute([
+                "sujet" => $sujet,
+                "message" => $message,
+                "date_limite" => $date_limite,
+                "etapes" => $etapes
+            ]);
+        }
     }
 } else {
     echo "Bien vouloir vous connecter avec d'effectuer cette tache";
@@ -36,19 +45,19 @@ require_once 'includes/header.php'
 <section>
 
     <div class="min-h-screen bg-[#B4CA65] flex items-center justify-center p-4">
-        <form class="bg-white rounded-2xl shadow-xl p-8 w-full max-w-xl space-y-6">
+        <form method="POST" class="bg-white rounded-2xl shadow-xl p-8 w-full max-w-xl space-y-6">
             <h2 class="text-2xl font-bold text-[#ff6c6c] text-center underline">Ajouter une tâche complexe</h2>
 
             <!-- Nom de la tâche -->
             <div>
-                <label class="block text-[#333] mb-1 font-semibold">Titre de la tâche</label>
-                <input type="text" name="titre" placeholder="Ex: Organiser une conférence"
+                <label class="block text-[#333] mb-1 font-semibold">sujet de la tâche</label>
+                <input type="text" name="sujet" placeholder="Ex: Organiser une conférence"
                     class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-2 focus:border-[#B4CA65] focus:text-gray-800 text-gray-700">
             </div>
 
-            <!-- Description -->
+            <!-- message -->
             <div>
-                <label class="block text-[#333] mb-1 font-semibold">Description</label>
+                <label class="block text-[#333] mb-1 font-semibold">message</label>
                 <textarea name="message" rows="4" placeholder="Détaille les étapes..."
                     class="resize-none w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-2 focus:border-[#B4CA65] focus:text-gray-800 text-gray-700"></textarea>
             </div>
