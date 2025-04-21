859f2a9e8b134ed83c965a5404ade5561e1ec4d6
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-21 13:06:29 +0200

    text

--- javascript/script_nav.js
+++ javascript/script_nav.js
@@ -1,12 +1,12 @@
 //menu toggle
-const btn = document.getElementById('menu-btn');
-const menu = document.getElementById('mobile-menu');
-btn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("mobile-menu");
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
 });
 
 //select menu
-document.getElementById('taskType').addEventListener('change', function() {
+document.getElementById("taskType").addEventListener("change", function () {
   const selectedUrl = this.value;
   if (selectedUrl) {
     window.location.href = selectedUrl;
