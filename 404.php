34b105db9c3914401d3991691ea184d565db48a0
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-21 17:01:18 +0200

    text

--- 404.php
+++ 404.php
@@ -1,9 +1,12 @@
 <?php
 require_once 'includes/header.php';
 ?>
-<h1>PAGE NOT FOUND</h1>
-
-
+<div class="flex justify-center items-center flex-col min-h-screen bg-[#B4CA65] gap-4">
+    <h1 class="text-3xl text-white">PAGE NOT FOUND</h1>
+    <div class="">
+        <img src="./assets/inages/not-found.jpg" alt="page_not_found">
+    </div>
+</div>
 
 
 
